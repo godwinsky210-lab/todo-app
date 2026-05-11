505170472dd2f84eb7c9e15b06f4fa071be2196f
Godwin Obidinma <godwinsky210@gmail.com>
2026-05-11 13:20:13 +0100

    Add Patch Todo functionality in main.js and update index.html

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ const todoPostTitleInputElement = document.getElementById("todoPostTitleInput");
 const todoPostButtonElement = document.getElementById("todoPostButton");
 const todoPostParagraphElement = document.getElementById("todoPostParagraph");
 
+// const todopatchTitleInputElement = document.getElementById(
+//   "todoPatchTitleInput",
+// );
 
 todoGetButtonElement.addEventListener("click", async function getTodo() {
   const todoId = todoGetInputElement.value;
@@ -49,4 +52,42 @@ todoPostButtonElement.addEventListener("click", async function postTodo() {
   } = todo;
   console.log({ todoTitle, todoUserId, todoId, todoCompleted });
   todoPostParagraphElement.textContent = `Hello👋🏿, I am User ${todoUserId}. My todo is ${todoTitle} and id is ${todoId}. It has ${todoCompleted ? "" : "not"} been completed`;
+});
+
+const todoPatchButtonElement = document.getElementById("todoPatchButton");
+const todoPatchParagraphElement = document.getElementById("todoPatchParagraph");
+const todoPatchUserIdInputElement = document.getElementById(
+  "todoPatchUserIdInputElement",
+);
+const todoPatchCompletedInputElement = document.getElementById(
+  "todoPatchCompletedInput",
+);
+const todoPatchTitleInputElement = document.getElementById(
+  "todoPatchTitleInput",
+);
+todoPatchButtonElement.addEventListener("click", async function PatchTodo() {
+  const userid = todoPatchUserIdInputElement.value;
+  const completed = todoPatchCompletedInputElement.checked;
+  const title = todoPatchTitleInputElement.value;
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      title,
+      userid,
+      completed,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  const todo = await response.json();
+  const {
+    title: todoTitle,
+    userid: todoUserId,
+    id: todoId,
+    completed: todoCompleted,
+  } = todo;
+  console.log({ todoTitle, todoUserId, todoId, todoCompleted });
+  todoPatchParagraphElement.textContent = `Hello👋🏿, I am User ${todoUserId}. My todo is ${todoTitle} and id is ${todoId}. It has ${todoCompleted ? "" : "not"} been completed`;
 });
